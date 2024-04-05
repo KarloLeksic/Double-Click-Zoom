719aeb4fac7b2526abc5bd3df02b24bae8334d95
Karlo Leksic <lekxka@gmail.com>
2024-04-05 08:30:51 +0200

    making functions for each event because on mobile phones, there are other events with the same functionality

--- script.js
+++ script.js
@@ -1,54 +1,20 @@
 const zoomMultiplier = 2;
 const container = document.getElementById('container');
 const image = document.getElementById('image');
-const containerPosition = container.getBoundingClientRect();
-const imagePosition = image.getBoundingClientRect(); //svugdje treba velicina zumirane slike
+const containerPositionInfo = container.getBoundingClientRect();
+const imagePositionInfo = image.getBoundingClientRect();
 
-const translateSpaceX = (imagePosition.width * zoomMultiplier - containerPosition.width) / 2;
-const translateSpaceY = (imagePosition.height * zoomMultiplier - containerPosition.height) / 2;
-
+const translateSpaceX = (imagePositionInfo.width * zoomMultiplier - containerPositionInfo.width) / 2;
+const translateSpaceY = (imagePositionInfo.height * zoomMultiplier - containerPositionInfo.height) / 2;
 
 let isZoomed = false;
-let mouseDown = false;
-let startX, startY, translateX = 0, translateY = 0;
-let zoomX = 0, zoomY = 0;
+let isMouseDown = false;
+let startX, startY, translateX, translateY;
 
-container.addEventListener('dblclick', (e) => {
-    if (isZoomed) {
-        image.style.transform = "scale(1)";
-        image.style.cursor = 'zoom-in';
-    } else {
-        translateX = scale(e.layerX, 0, containerPosition.width, translateSpaceX, -translateSpaceX);
-        translateY = scale(e.layerY, 0, containerPosition.height, translateSpaceY, -translateSpaceY);
-
-        translate();
-
-        image.style.cursor = 'zoom-out';
-    }
-
-    isZoomed = !isZoomed;
-});
-
-container.addEventListener('mousedown', (e) => {
-    e.preventDefault();
-    mouseDown = true;
-
-    startX = e.clientX - translateX;
-    startY = e.clientY - translateY;
-});
-
-document.addEventListener('mousemove', (e) => {
-    if (isZoomed && mouseDown) {
-        translateX = Math.min(Math.max(e.clientX - startX, -translateSpaceX), translateSpaceX);
-        translateY = Math.min(Math.max(e.clientY - startY, -translateSpaceY), translateSpaceY);
-
-        translate();
-    }
-});
-
-document.addEventListener('mouseup', () => {
-    mouseDown = false;
-});
+container.addEventListener('dblclick', (e) => doubleClick(e));
+container.addEventListener('mousedown', (e) => mouseDown(e));
+document.addEventListener('mousemove', (e) => mouseMove(e));
+document.addEventListener('mouseup', () => isMouseDown = false);
 
 function translate() {
     image.style.transform = `scale(${zoomMultiplier}) translate(${translateX / zoomMultiplier}px, ${translateY / zoomMultiplier}px)`;
@@ -58,13 +24,14 @@ function scale(x, in_min, in_max, out_min, out_max) {
     return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
 
+// Functions for events because events are different for mobile devices
 function doubleClick(e) {
     if (isZoomed) {
         image.style.transform = "scale(1)";
         image.style.cursor = 'zoom-in';
     } else {
-        translateX = scale(e.layerX, 0, containerPosition.width, translateSpaceX, -translateSpaceX);
-        translateY = scale(e.layerY, 0, containerPosition.height, translateSpaceY, -translateSpaceY);
+        translateX = scale(e.layerX, 0, containerPositionInfo.width, translateSpaceX, -translateSpaceX);
+        translateY = scale(e.layerY, 0, containerPositionInfo.height, translateSpaceY, -translateSpaceY);
 
         translate();
 
@@ -72,4 +39,21 @@ function doubleClick(e) {
     }
 
     isZoomed = !isZoomed;
+}
+
+function mouseDown(e) {
+    e.preventDefault();
+    isMouseDown = true;
+
+    startX = e.clientX - translateX;
+    startY = e.clientY - translateY;
+}
+
+function mouseMove(e) {
+    if (isZoomed && isMouseDown) {
+        translateX = Math.min(Math.max(e.clientX - startX, -translateSpaceX), translateSpaceX);
+        translateY = Math.min(Math.max(e.clientY - startY, -translateSpaceY), translateSpaceY);
+
+        translate();
+    }
 }
