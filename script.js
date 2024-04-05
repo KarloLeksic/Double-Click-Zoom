7ec9d64e773476c7a474b6ed07f09cc153dcd77a
Karlo Leksic <lekxka@gmail.com>
2024-04-05 12:31:09 +0200

    everything works, but better written

--- script.js
+++ script.js
@@ -1,37 +1,27 @@
 const zoomMultiplier = 2;
 const container = document.getElementById('container');
 const image = document.getElementById('image');
-const containerPositionInfo = container.getBoundingClientRect();
-const imagePositionInfo = image.getBoundingClientRect();
 
-const translateSpaceX = (imagePositionInfo.width * zoomMultiplier - containerPositionInfo.width) / 2;
-const translateSpaceY = (imagePositionInfo.height * zoomMultiplier - containerPositionInfo.height) / 2;
+let translateSpaceX = 0;
+let translateSpaceY = 0;
 
 let isZoomed = false;
-let isMouseDown = false;
+let mouseDown = false;
 let startX, startY, translateX, translateY;
 
-container.addEventListener('dblclick', (e) => doubleClick(e));
-container.addEventListener('mousedown', (e) => mouseDown(e));
-document.addEventListener('mousemove', (e) => mouseMove(e));
-document.addEventListener('mouseup', () => isMouseDown = false);
+image.addEventListener('dblclick', (e) => {
+    const containerPosition = container.getBoundingClientRect();
+    const imagePosition = image.getBoundingClientRect();
 
-function translate() {
-    image.style.transform = `scale(${zoomMultiplier}) translate(${translateX / zoomMultiplier}px, ${translateY / zoomMultiplier}px)`;
-}
+    translateSpaceX = (imagePosition.width * zoomMultiplier - containerPosition.width) / 2;
+    translateSpaceY = (imagePosition.height * zoomMultiplier - containerPosition.height) / 2;
 
-function scale(x, in_min, in_max, out_min, out_max) {
-    return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
-}
-
-// Functions for events because events are different for mobile devices
-function doubleClick(e) {
     if (isZoomed) {
         image.style.transform = "scale(1)";
         image.style.cursor = 'zoom-in';
     } else {
-        translateX = scale(e.layerX, 0, imagePositionInfo.width, translateSpaceX, -translateSpaceX);
-        translateY = scale(e.layerY, 0, imagePositionInfo.height, translateSpaceY, -translateSpaceY);
+        translateX = scale(e.layerX, 0, containerPosition.width, translateSpaceX, -translateSpaceX);
+        translateY = scale(e.layerY, 0, containerPosition.height, translateSpaceY, -translateSpaceY);
 
         translate();
 
@@ -39,21 +29,33 @@ function doubleClick(e) {
     }
 
     isZoomed = !isZoomed;
-}
+});
 
-function mouseDown(e) {
+container.addEventListener('mousedown', (e) => {
     e.preventDefault();
-    isMouseDown = true;
+    mouseDown = true;
 
     startX = e.clientX - translateX;
     startY = e.clientY - translateY;
-}
+});
 
-function mouseMove(e) {
-    if (isZoomed && isMouseDown) {
+document.addEventListener('mousemove', (e) => {
+    if (isZoomed && mouseDown) {
         translateX = Math.min(Math.max(e.clientX - startX, -translateSpaceX), translateSpaceX);
         translateY = Math.min(Math.max(e.clientY - startY, -translateSpaceY), translateSpaceY);
 
         translate();
     }
+});
+
+document.addEventListener('mouseup', () => {
+    mouseDown = false;
+});
+
+function translate() {
+    image.style.transform = `scale(${zoomMultiplier}) translate(${translateX / zoomMultiplier}px, ${translateY / zoomMultiplier}px)`;
+}
+
+function scale(x, in_min, in_max, out_min, out_max) {
+    return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
